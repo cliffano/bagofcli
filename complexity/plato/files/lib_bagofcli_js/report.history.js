3cf27bdc5f07f89f931d5be47db792cf7928f509
cliffano <cliffano@users.noreply.github.com>
2022-01-29 05:41:16 +0000

    deploy: d49be8819193805e931379772f6bfc186976bfe7

--- complexity/plato/files/lib_bagofcli_js/report.history.js
+++ complexity/plato/files/lib_bagofcli_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Sat, 29 Jan 2022 05:41:08 GMT","sloc":552,"lloc":240,"functions":49,"deliveredBugs":4.221,"difficulty":71.899,"maintainability":71.187,"lintErrors":23}]
+__history = [{"date":"Sat, 29 Jan 2022 05:41:10 GMT","sloc":552,"lloc":240,"functions":49,"deliveredBugs":4.221,"difficulty":71.899,"maintainability":71.187,"lintErrors":23}]
